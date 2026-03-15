a6379850f428824c5befd5660bfb685f29f6a49e
ekoporta2215 <ekoporta2215@gmail.com>
2026-03-15 21:40:36 +0000

    Prueba Corta 1

---
+++ programa.c
@@ -0,0 +1,16 @@
+/*Erick Oporta Gonzalez*/
+
+#include <stdio.h>
+int main(){
+    int identificacion;
+    char nombreCompleto[] = "Oporta Gonzalez Erick";
+    
+    printf("Ingrese su numero de identificaion con 0, si aplica: ");
+    scanf("%d", &identificacion);
+
+    printf("Nombre completo: %s\n", nombreCompleto);
+
+    return 0;
+}
+
+/*Erick Oporta Gonzalez*/
---
+++ programa.py
@@ -0,0 +1,8 @@
+# Erick Oporta Gonzalez
+
+nombre_completo = "Oporta Gonzalez Erick"
+identificacion = int(input("Ingrese su identificaion con 0, si aplica: "))
+
+print ("Nombre completo: ", nombre_completo)
+
+# Erick Oporta Gonzalez
